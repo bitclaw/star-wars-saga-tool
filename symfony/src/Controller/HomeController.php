8068ba2069162b531eef3a4f26034080125aebfb
Daniel C <bitclaw@gmail.com>
2021-05-15 14:56:14 -0500

    Add login screen #3
    * HomeController should only be accessed by authenticated users.

--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -2,12 +2,14 @@
 // src/Controller/HomeController.php
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController
 {
     /**
+     * @IsGranted("ROLE_USER")
      * @Route("/", name="app_homepage")
      */
     public function index()
