9cf71e6fd231cadc0c8ae13683d9ad711f51c089
Daniel C <bitclaw@gmail.com>
2021-05-15 12:42:46 -0500

    Add login screen #3
    * Add fixture data for one user.

--- symfony/src/DataFixtures/UserFixture.php
+++ symfony/src/DataFixtures/UserFixture.php
@@ -2,15 +2,28 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setUsername('user');
+        $user->setEmail('user@gmail.com');
+        $password = $this->encoder->encodePassword($user, '123456789');
+        $user->setPassword($password);
+        $manager->persist($user);
 
         $manager->flush();
     }
