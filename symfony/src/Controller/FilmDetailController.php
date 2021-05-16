8080c2e4dfa7a6ffe53a5524f4d12c4b8805b51c
Daniel C <bitclaw@gmail.com>
2021-05-16 02:17:30 -0500

    Full list of Star Wars films #4
    * Get film with id from url sent from the film master page.

--- symfony/src/Controller/FilmDetailController.php
+++ symfony/src/Controller/FilmDetailController.php
@@ -3,13 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Film as FilmEntity;
+use App\Service\Film;
+use App\Service\Swapi;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Service\Swapi;
-use App\Service\Film;
 
 class FilmDetailController extends AbstractController
 {
@@ -17,8 +17,12 @@ class FilmDetailController extends AbstractController
      * @IsGranted("ROLE_USER")
      * @Route("/film-detail/{id}",defaults={"id" = 1}, name="film_detail")
      */
-    public function index(Request $request)
+    public function index(Request $request, EntityManagerInterface $entityManager, Swapi $swapi, Film $filmService)
     {
+        $id = (int)$request->get('id');
+        $repository = $entityManager->getRepository(FilmEntity::class);
+        $film = $repository->find($id);
+
         return $this->render('film/detail.html.twig', [
             'detail' => '',
         ]);
