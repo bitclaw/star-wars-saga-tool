f0e96354cbdc00a51a91fdfbad1048e5de46708e
Daniel C <bitclaw@gmail.com>
2021-05-16 07:18:39 -0500

    Full list of Star Wars films #4
    * Send file object over to the detail.html.twig view to render info. Also fixed error in comment.
    * Clean up repository classes.
    * Render some of the Film object attributes on the detail.html.twig to make sure the info is rendering ok.

--- symfony/src/Controller/FilmDetailController.php
+++ symfony/src/Controller/FilmDetailController.php
@@ -1,10 +1,8 @@
 <?php
-// src/Controller/HomeController.php
+// src/Controller/FilmDetail.php
 namespace App\Controller;
 
 use App\Entity\Film as FilmEntity;
-use App\Service\Film;
-use App\Service\Swapi;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,14 +15,14 @@ class FilmDetailController extends AbstractController
      * @IsGranted("ROLE_USER")
      * @Route("/film-detail/{id}",defaults={"id" = 1}, name="film_detail")
      */
-    public function index(Request $request, EntityManagerInterface $entityManager, Swapi $swapi, Film $filmService)
+    public function index(Request $request, EntityManagerInterface $entityManager)
     {
         $id = (int)$request->get('id');
         $repository = $entityManager->getRepository(FilmEntity::class);
         $film = $repository->find($id);
 
         return $this->render('film/detail.html.twig', [
-            'detail' => '',
+            'film' => $film,
         ]);
     }
 }
--- symfony/src/Repository/FilmRepository.php
+++ symfony/src/Repository/FilmRepository.php
@@ -5,7 +5,6 @@ namespace App\Repository;
 use App\Entity\Film;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Doctrine\Persistence\ObjectManager;
 
 /**
  * @method Film|null find($id, $lockMode = null, $lockVersion = null)
@@ -18,6 +17,5 @@ class FilmRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Film::class);
-
     }
 }
