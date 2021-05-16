7225a51a8dd5b42399c728aa3fb699b9d70f7076
Daniel C <bitclaw@gmail.com>
2021-05-16 00:51:50 -0500

    Full list of Star Wars films #4
    * Run `php bin/console doctrine:migrations:diff` to generate migration from Entity\Film. Don't
      forget to run `php bin/console doctrine:migrations:migrate` to run migration after creating it.

---
+++ symfony/migrations/Version20210516055027.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210516055027 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE film (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, episode_id INT NOT NULL, opening_crawl LONGTEXT NOT NULL, director VARCHAR(255) NOT NULL, producer VARCHAR(255) NOT NULL, release_date VARCHAR(255) NOT NULL, characters JSON NOT NULL, planets JSON NOT NULL, starships JSON NOT NULL, vehicles JSON NOT NULL, species JSON NOT NULL, url VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE film');
+    }
+}
