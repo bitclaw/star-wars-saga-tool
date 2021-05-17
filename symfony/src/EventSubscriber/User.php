bde09f0431958064099a1cc30c210ca5dab3d923
Daniel C <bitclaw@gmail.com>
2021-05-17 09:56:13 -0500

    Send an email notification every time a user logs in #6
    * Finish up logic in event subscriber to send email. Will test with mailtrap.io.
    * Verified event subscriber with the command `php bin/console debug:event-dispatcher | grep 'onLoginSuccess'`.

--- symfony/src/EventSubscriber/User.php
+++ symfony/src/EventSubscriber/User.php
@@ -2,29 +2,35 @@
 
 namespace App\EventSubscriber;
 
-use App\Entity\Setting as SettingEntity;
+use App\Entity\Setting;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 class User implements EventSubscriberInterface
 {
     private $em;
+    /**
+     * @var MailerInterface
+     */
+    private $mailer;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, MailerInterface $mailer)
     {
         $this->em = $em;
+        $this->mailer = $mailer;
     }
 
     public function onLoginSuccess(LoginSuccessEvent $event)
     {
         $user = $event->getUser();
-        $repository = $this->em->getRepository(SettingEntity::class);
         $setting = $this->em->getRepository('App\Entity\Setting')->findOneBy([
             'name' => 'email',
         ]);
-
-        $this->em->flush();
+        $this->sendEmail($user, $setting);
     }
 
     public static function getSubscribedEvents()
@@ -32,5 +38,18 @@ class User implements EventSubscriberInterface
         return [
             LoginSuccessEvent::class => 'onLoginSuccess'
         ];
+    }
+
+    private function sendEmail(UserInterface $user, Setting $setting): void
+    {
+        $email = (new Email())
+            ->from($setting->getValue())
+            ->to($user->getEmail())
+            ->priority(Email::PRIORITY_HIGH)
+            ->subject('User logged in...')
+            ->text('User logged in successfully!')
+            ->html('<p>User logged in successfully!</p>');
+
+        $this->mailer->send($email);
     }
 }
